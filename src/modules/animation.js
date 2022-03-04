5a37c963145600f71169372532d233eb887af8a3
Elerqsousy <mahmoud.eleresousy@gmail.com>
2022-03-04 13:58:43 +0200

    Added animation file containing animation object

---
+++ src/modules/animation.js
@@ -0,0 +1,14 @@
+const animation = document.createElement('div');
+animation.classList.add('loading');
+animation.innerHTML =
+  '<div class="loading-text">' +
+  '<span class="loading-text-words">L</span>' +
+  '<span class="loading-text-words">O</span>' +
+  '<span class="loading-text-words">A</span>' +
+  '<span class="loading-text-words">D</span>' +
+  '<span class="loading-text-words">I</span>' +
+  '<span class="loading-text-words">N</span>' +
+  '<span class="loading-text-words">G</span>' +
+  '</div>';
+
+export default animation;
