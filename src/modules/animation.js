dbda7554b401575bf8f06a12f17f198fd9bf0c24
Elerqsousy <mahmoud.eleresousy@gmail.com>
2022-03-05 02:42:37 +0200

    Add animation object

--- src/modules/animation.js
+++ src/modules/animation.js
@@ -1,14 +1,12 @@
-const animation = document.createElement('div');
-animation.classList.add('loading');
-animation.innerHTML =
-  '<div class="loading-text">' +
-  '<span class="loading-text-words">L</span>' +
-  '<span class="loading-text-words">O</span>' +
-  '<span class="loading-text-words">A</span>' +
-  '<span class="loading-text-words">D</span>' +
-  '<span class="loading-text-words">I</span>' +
-  '<span class="loading-text-words">N</span>' +
-  '<span class="loading-text-words">G</span>' +
-  '</div>';
+const animation = '<div class="loading">'
+  + '<div class="loading-text">'
+  + '<span class="loading-text-words">L</span>'
+  + '<span class="loading-text-words">O</span>'
+  + '<span class="loading-text-words">A</span>'
+  + '<span class="loading-text-words">D</span>'
+  + '<span class="loading-text-words">I</span>'
+  + '<span class="loading-text-words">N</span>'
+  + '<span class="loading-text-words">G</span>'
+  + '</div></div>';
 
 export default animation;
