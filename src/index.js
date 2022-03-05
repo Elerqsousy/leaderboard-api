9b46becd7f2dee4b893667ec7f39ef7a04d22579
Elerqsousy <mahmoud.eleresousy@gmail.com>
2022-03-05 02:51:07 +0200

    Update event listeners to use api push and get

--- src/index.js
+++ src/index.js
@@ -1,32 +1,52 @@
 import './animation.css';
 import './style.css';
-import animation from "./modules/animation"
-import Data from './modules/data.js';
-
-const list = new Data();
+import animation from './modules/animation.js';
+import postToAPI from './modules/post-score.js';
+import getFromAPI from './modules/get-scores.js';
 
 const addForm = document.querySelector('.form');
 const listContainer = document.querySelector('.scores-list');
-const animationContainer = document.querySelector('.message')
-// animationContainer.appendChild(animation)
+const animationContainer = document.querySelectorAll('.message');
+const refreshBtn = document.querySelector('.refresh');
 
-const displayItems = () => {
+const gameID = 'L5WwHwN7zdChbtO2i0g9';
+const baseURL = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameID}/scores/`;
+
+addForm.addEventListener('submit', (e) => {
+  const name = e.target[0].value;
+  const score = e.target[1].value;
+  postToAPI(name, score, baseURL, animationContainer[1], animation);
+  e.target[0].value = '';
+  e.target[1].value = '';
+  e.preventDefault();
+});
+
+const displayItems = (list) => {
   listContainer.innerHTML = '';
-  list.list.forEach((e, index) => {
+
+  list.forEach((e, index) => {
     const item = document.createElement('li');
     item.classList.add('list-item');
     if (index % 2) {
       item.classList.add('dark-bg');
     }
-    item.innerHTML = `${e.name}: ${e.score}`;
+    item.innerHTML = `${e.user}: ${e.score}`;
     listContainer.appendChild(item);
   });
+  animationContainer[0].innerHTML = 'Scores updates successfuly!';
+  setTimeout(() => {
+    animationContainer[0].innerHTML = '';
+  }, 4000);
 };
 
-addForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const name = e.target[0].value;
-  const score = e.target[1].value;
-  list.add(name, score);
-  displayItems();
+const callAPI = async () => {
+  const list = await getFromAPI(baseURL, animationContainer[0], animation);
+  displayItems(list);
+};
+
+refreshBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  callAPI();
 });
+
+callAPI();
